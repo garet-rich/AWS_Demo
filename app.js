8f00138142ba43f10d94cc0aa651c154fb68ae53
Garet Rich <garet.rich@gmail.com>
2021-08-18 12:33:53 -0600

    Updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Express myself!'));
+app.get('/', (req, res) => res.send('Express myself! Total tutorial completion.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
